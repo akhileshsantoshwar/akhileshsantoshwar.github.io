f32058ce03af552bd058828e49f26875dfd68271
Akhilesh Santoshwar <58439678+akhileshsantoshwar@users.noreply.github.com>
2022-03-01 15:22:15 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -2,18 +2,18 @@
 
  
 $name = $_POST['name'];
+$phone = $_POST['phone'];
 $email = $_POST['email'];
-$sub = $_POST['sub'];
 $message = $_POST['message'];
 $to = "akhileshsantoshwarofficial.com";
 $subject = "Mail from website";
 
 $header = "From ".$name. "\r\n" .
 
-$txt = "Name: ".$name."\r\nEmail: ".$email."\r\nsubject: ".$sub."\r\message: ".$message;
+$txt = "Name: ".$name."\r\nEmail: ".$email."\r\nMobile: ".$phone."\r\message: ".$message;
 
 if($email!=NULL){
-	mail($to, $sub, $txt, $header);
+	mail($to, $phone, $txt, $header);
 }
 
 
